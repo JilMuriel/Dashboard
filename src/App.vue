3579178f26e13efc633097c48c44513bd3090fc8
JilMuriel <djibrilmuriel@gmail.com>
2020-03-11 11:40:57 +0800

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
   <div id="app">
-
-
+    <Navbar />
   </div>
 </template>
 
 <script>
-
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-
+    Navbar
   }
 }
 </script>
 
 <style lang="scss">
   @import url('https://fonts.googleapis.com/css?family=Poppins&display=swap');
-  $body: #ffff;
+  html {
+    font-size: 16px;
+    font-family: 'Poppins', sans-serif;
+  }
+  * {
+    margin: 0px;
+  }
   body {
     box-sizing: border-box;
     margin: 0;
-    font-family: 'Poppins', sans-serif;
-    background: $body;
   }
-  
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <h1>{{ title }}</h1>
+    </nav>
+</template>
+<script>
+export default {
+    data: () => {
+        title: 'Hello from Header',
+    },
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
